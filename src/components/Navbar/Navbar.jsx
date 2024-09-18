0ade3985dbe4d1c608a2b68cd500a2b96ba4b603
Damilola-FullStackDeveloper <bakarewealth@gmail.com>
2024-09-18 11:12:47 +0100

    change the background color to black

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import './Navbar.css'
-import logo from '../../assets/logo.jpg'
+import logo from '../../assets/logo.jpeg'
 import arrow_icon from '../../assets/arrow_icon.png'
 import { CoinContext } from '../../context/CoinContext'
 import { Link } from 'react-router-dom'
